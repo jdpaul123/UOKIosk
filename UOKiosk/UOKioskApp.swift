ec4a86517cbb4ddeea3b5a9f450eb55f6e8dbeaf
JD Paul <jdpaul123@gmail.com>
2022-04-15 14:32:58 -0700

    initial commit. I set up the tabs and view files

---
+++ UOKiosk/UOKioskApp.swift
@@ -0,0 +1,17 @@
+//
+//  UOKioskApp.swift
+//  UOKiosk
+//
+//  Created by Jonathan Paul on 4/15/22.
+//
+
+import SwiftUI
+
+@main
+struct UOKioskApp: App {
+    var body: some Scene {
+        WindowGroup {
+            TabOptionsView()
+        }
+    }
+}
---
+++ UOKiosk/Views/NewsView.swift
@@ -0,0 +1,8 @@
+//
+//  NewsUI.swift
+//  UOKiosk
+//
+//  Created by Jonathan Paul on 4/15/22.
+//
+
+import Foundation
