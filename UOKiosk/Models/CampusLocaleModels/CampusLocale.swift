11bbed68be6b18ea19860308186e174e1131d0af
JD Paul <jdpaul123@gmail.com>
2023-05-15 16:10:35 -0700

    Created two new CoreData Entities for on campus stores/facilities and another for their hours

---
+++ UOKiosk/Models/CampusLocaleModels/CampusLocale.swift
@@ -0,0 +1,15 @@
+//
+//  CampusLocale+CoreDataClass.swift
+//  UOKiosk
+//
+//  Created by Jonathan Paul on 5/15/23.
+//
+//
+
+import Foundation
+import CoreData
+
+@objc(CampusLocale)
+public class CampusLocale: NSManagedObject {
+
+}
---
+++ UOKiosk/Models/CampusLocaleModels/HoursOfOperation.swift
@@ -0,0 +1,15 @@
+//
+//  HoursOfOperation+CoreDataClass.swift
+//  UOKiosk
+//
+//  Created by Jonathan Paul on 5/15/23.
+//
+//
+
+import Foundation
+import CoreData
+
+@objc(HoursOfOperation)
+public class HoursOfOperation: NSManagedObject {
+
+}
