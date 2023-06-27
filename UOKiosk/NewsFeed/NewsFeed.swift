a24c7c1c0e33bf276f2825c41ca607231f1a5804
JD Paul <jdpaul123@gmail.com>
2023-06-27 13:28:40 -0700

    Created New Feed group, view, and tab

---
+++ UOKiosk/NewsFeed/NewsFeed.swift
@@ -0,0 +1,20 @@
+//
+//  NewsFeed.swift
+//  UOKiosk
+//
+//  Created by Jonathan Paul on 6/27/23.
+//
+
+import SwiftUI
+
+struct NewsFeed: View {
+    var body: some View {
+        Text("News Feed")
+    }
+}
+
+struct NewsFeed_Previews: PreviewProvider {
+    static var previews: some View {
+        NewsFeed()
+    }
+}
