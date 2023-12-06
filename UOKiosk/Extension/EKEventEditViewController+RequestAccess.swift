aa601fd5be0e89ee14043e35340e5f2970380e3b
JD Paul <jdpaul123@gmail.com>
2023-12-06 12:21:44 -0800

    Delted old, unused, methods for requesting access to add events to the calendar. Moved the EKEventEditViewController extension that includes the requestAccess() method and an overridden viewWillAppear() that calls the new requestAccess() method to it's own file in the Extension group

---
+++ UOKiosk/Extension/EKEventEditViewController+RequestAccess.swift
@@ -0,0 +1,57 @@
+//
+//  EKEventEditViewController+RequestAccess.swift
+//  UOKiosk
+//
+//  Created by Jonathan Paul on 12/6/23.
+//
+
+import Foundation
+import EventKitUI
+
+extension EKEventEditViewController {
+    open override func viewDidAppear(_ animated: Bool) {
+        Task {
+            do {
+                let success = try await requestAccess()
+                if success {
+                    print("SUCCESS REQUESTING ACCESS TO ADD AN EVENT")
+                } else {
+                    print("FAILED TO GET ACCESS TO ADD AN EVENT WITH NO ERROR")
+                }
+            } catch {
+                // TODO: Log the error
+            }
+        }
+        super.viewDidAppear(animated)
+    }
+
+    @discardableResult
+    func requestAccess() async throws -> Bool {
+        let status = EKEventStore.authorizationStatus(for: .event)
+        switch status {
+        case .notDetermined, .denied:
+            if #available(iOS 17.0, *) {
+                // TODO: Is there a bug in .requestWriteOnlyAccessToEvents? When I use it and deny access in iOS 17, I am still able to add events to the calendar
+                do {
+                    let result = try await eventStore.requestWriteOnlyAccessToEvents()
+                    return result
+                } catch {
+                    throw error
+                }
+            } else {
+                do {
+                    let result = try await eventStore.requestAccess(to: .event)
+                    return result
+                } catch {
+                    throw error
+                }
+            }
+        case .fullAccess, .authorized, .writeOnly:
+            return true
+        case .restricted:
+            throw PermissionError.accessRestricted
+        @unknown default:
+            throw PermissionError.unknown
+        }
+    }
+}
