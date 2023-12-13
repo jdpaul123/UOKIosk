f206646881672558e7a13518a1ad4f5bfde0fb4b
JD Paul <jdpaul123@gmail.com>
2023-12-13 15:11:50 -0800

    Commented out AVAudioSession's call to setCategory() because it is causing the error log and I don't have a solution yet, plus the radio feature is not included in the production app right now anyway

--- UOKiosk/UOKioskApp.swift
+++ UOKiosk/UOKioskApp.swift
@@ -13,19 +13,13 @@ class AppDelegate: NSObject, UIApplicationDelegate {
                    didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey : Any]? = nil) -> Bool {
       FirebaseApp.configure()
 
-      // FIXME: AVFoundation causing error logging:
-      /*
-       AddInstanceForFactory: No factory registered for id <CFUUID 0x600000238d40> F8BB1C28-BAE8-11D6-9C31-00039315CD46
-       nw_connection_add_timestamp_locked_on_nw_queue [C1] Hit maximum timestamp count, will start dropping events
-       nw_socket_handle_socket_event [C2.1.1.1:3] Socket SO_ERROR [54: Connection reset by peer]
-       nw_read_request_report [C2] Receive failed with error "Connection reset by peer"
-       */
+      // FIXME: AVFoundation causing error logging: AddInstanceForFactory: No factory registered for id <CFUUID 0x600000238d40> F8BB1C28-BAE8-11D6-9C31-00039315CD46
       // https://stackoverflow.com/questions/58360765/swift-5-1-error-plugin-addinstanceforfactory-no-factory-registered-for-id-c
-      do {
-          try AVAudioSession.sharedInstance().setCategory(.playback, mode: .default, options: [/*.mixWithOthers, .allowAirPlay*/])
-      } catch {
-          print("Failed to set audio session category.")
-      }
+//      do {
+//          try AVAudioSession.sharedInstance().setCategory(.playback, mode: .default, options: [])
+//      } catch {
+//          print("Failed to set audio session category. With error: \(error.localizedDescription)")
+//      }
       return true
   }
 }
